ca95d3c1232a14c3d01f3ada4c24c230ec9828f7
GuillePfarr <105952072+GuillePfarr@users.noreply.github.com>
2023-06-27 20:35:22 -0300

    Modifico js

--- script.js
+++ script.js
@@ -1,77 +1,116 @@
-const $canvas = document.querySelector("#canvas"),
-    $btnDescargar = document.querySelector("#btnDescargar"),
-    $btnLimpiar = document.querySelector("#btnLimpiar"),
-    $btnGenerarDocumento = document.querySelector("#btnGenerarDocumento");
+const $canvas = document.querySelector("#canvas");
+const $btnDescargar = document.querySelector("#btnDescargar");
+const $btnLimpiar = document.querySelector("#btnLimpiar");
+const $btnGenerarDocumento = document.querySelector("#btnGenerarDocumento");
 const contexto = $canvas.getContext("2d");
 const COLOR_PINCEL = "black";
 const COLOR_FONDO = "white";
 const GROSOR = 2;
 let xAnterior = 0, yAnterior = 0, xActual = 0, yActual = 0;
-const obtenerXReal = (clientX) => clientX - $canvas.getBoundingClientRect().left;
-const obtenerYReal = (clientY) => clientY - $canvas.getBoundingClientRect().top;
-let haComenzadoDibujo = false; // Bandera que indica si el usuario está presionando el botón del mouse sin soltarlo
+let haComenzadoDibujo = false;
 
+const obtenerXReal = (clientX) =>
+  clientX - $canvas.getBoundingClientRect().left;
+const obtenerYReal = (clientY) =>
+  clientY - $canvas.getBoundingClientRect().top;
 
 const limpiarCanvas = () => {
-    // Colocar color blanco en fondo de canvas
-    contexto.fillStyle = COLOR_FONDO;
-    contexto.fillRect(0, 0, $canvas.width, $canvas.height);
+  contexto.fillStyle = COLOR_FONDO;
+  contexto.fillRect(0, 0, $canvas.width, $canvas.height);
 };
+
 limpiarCanvas();
+
 $btnLimpiar.onclick = limpiarCanvas;
-// Escuchar clic del botón para descargar el canvas
+
 $btnDescargar.onclick = () => {
-    const enlace = document.createElement('a');
-    // El título
-    enlace.download = "Firma.png";
-    // Convertir la imagen a Base64 y ponerlo en el enlace
-    enlace.href = $canvas.toDataURL();
-    // Hacer click en él
-    enlace.click();
+  const enlace = document.createElement("a");
+  enlace.download = "Firma.png";
+  enlace.href = $canvas.toDataURL();
+  enlace.click();
 };
 
 window.obtenerImagen = () => {
-    return $canvas.toDataURL();
+  return $canvas.toDataURL();
 };
 
 $btnGenerarDocumento.onclick = () => {
-    window.open("doc.html");
+  window.open("doc.html");
 };
-// Lo demás tiene que ver con pintar sobre el canvas en los eventos del mouse
-$canvas.addEventListener("mousedown", evento => {
-    // En este evento solo se ha iniciado el clic, así que dibujamos un punto
-    xAnterior = xActual;
-    yAnterior = yActual;
-    xActual = obtenerXReal(evento.clientX);
-    yActual = obtenerYReal(evento.clientY);
-    contexto.beginPath();
-    contexto.fillStyle = COLOR_PINCEL;
-    contexto.fillRect(xActual, yActual, GROSOR, GROSOR);
-    contexto.closePath();
-    // Y establecemos la bandera
-    haComenzadoDibujo = true;
+
+$canvas.addEventListener("mousedown", (evento) => {
+  xAnterior = xActual;
+  yAnterior = yActual;
+  xActual = obtenerXReal(evento.clientX);
+  yActual = obtenerYReal(evento.clientY);
+  contexto.beginPath();
+  contexto.fillStyle = COLOR_PINCEL;
+  contexto.fillRect(xActual, yActual, GROSOR, GROSOR);
+  contexto.closePath();
+  haComenzadoDibujo = true;
 });
 
 $canvas.addEventListener("mousemove", (evento) => {
-    if (!haComenzadoDibujo) {
-        return;
-    }
-    // El mouse se está moviendo y el usuario está presionando el botón, así que dibujamos todo
-
-    xAnterior = xActual;
-    yAnterior = yActual;
-    xActual = obtenerXReal(evento.clientX);
-    yActual = obtenerYReal(evento.clientY);
-    contexto.beginPath();
-    contexto.moveTo(xAnterior, yAnterior);
-    contexto.lineTo(xActual, yActual);
-    contexto.strokeStyle = COLOR_PINCEL;
-    contexto.lineWidth = GROSOR;
-    contexto.stroke();
-    contexto.closePath();
+  if (!haComenzadoDibujo) {
+    return;
+  }
+  xAnterior = xActual;
+  yAnterior = yActual;
+  xActual = obtenerXReal(evento.clientX);
+  yActual = obtenerYReal(evento.clientY);
+  contexto.beginPath();
+  contexto.moveTo(xAnterior, yAnterior);
+  contexto.lineTo(xActual, yActual);
+  contexto.strokeStyle = COLOR_PINCEL;
+  contexto.lineWidth = GROSOR;
+  contexto.stroke();
+  contexto.closePath();
 });
-["mouseup", "mouseout"].forEach(nombreDeEvento => {
-    $canvas.addEventListener(nombreDeEvento, () => {
-        haComenzadoDibujo = false;
-    });
+
+$canvas.addEventListener("mouseup", () => {
+  haComenzadoDibujo = false;
+});
+
+$canvas.addEventListener("mouseout", () => {
+  haComenzadoDibujo = false;
+});
+
+$canvas.addEventListener("touchstart", (evento) => {
+  const touch = new Touch({
+    identifier: evento.touches[0].identifier,
+    target: $canvas,
+    clientX: obtenerXReal(evento.touches[0].clientX),
+    clientY: obtenerYReal(evento.touches[0].clientY),
+    pageX: evento.touches[0].pageX,
+    pageY: evento.touches[0].pageY,
+  });
+  const touchEvent = new TouchEvent("touchstart", {
+    touches: [touch],
+    targetTouches: [touch],
+    changedTouches: [touch],
+    bubbles: true,
+    cancelable: true,
+    composed: true,
+  });
+  $canvas.dispatchEvent(touchEvent);
+});
+
+$canvas.addEventListener("touchmove", (evento) => {
+  const touch = new Touch({
+    identifier: evento.touches[0].identifier,
+    target: $canvas,
+    clientX: obtenerXReal(evento.touches[0].clientX),
+    clientY: obtenerYReal(evento.touches[0].clientY),
+    pageX: evento.touches[0].pageX,
+    pageY: evento.touches[0].pageY,
+  });
+  const touchEvent = new TouchEvent("touchmove", {
+    touches: [touch],
+    targetTouches: [touch],
+    changedTouches: [touch],
+    bubbles: true,
+    cancelable: true,
+    composed: true,
+  });
+  $canvas.dispatchEvent(touchEvent);
 });
